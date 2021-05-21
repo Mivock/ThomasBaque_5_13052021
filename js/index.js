6d652fee2896c055127fc4bd0d96eed70a224122
Mivock <76434164+Mivock@users.noreply.github.com>
2021-05-21 17:05:39 +0200

    Récupération des data de l'API et création d'une carte par élément

--- js/index.js
+++ js/index.js
@@ -1,57 +1,39 @@
+
 // Récuperer les données de l'API
 
-async function getCameras() {
+    async function getCameras() {
 
-    const url = "http://localhost:3000/api/cameras"
-
-        fetch(url)
-        .then(function(res) {
-            if (res.ok) {
-                return res.json()
-            }
-        })
-        .then(function(data) {
-            
-            document.getElementById("productName").innerText = data[0].name;
-            document.getElementById("productInfo").innerText = data[0].description;
-            document.getElementById("productPrice").innerText = data[0].price;
-            document.getElementById("productImg").src = data[0].imageUrl;
-
-        })
-        .catch(function(err) {
-            // message d'erreur
-        }) 
-}
-
-// Création card HTML pour produits
-
-function createCards() {
-
-    let productCard = 
-        `<a href="#">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title"><span class="" id="productName"></span></h5>
-                <p class="card-text"><span class="" id="productInfo"></span></p>
-                <p class="card-text"><small class="text-muted"><span class="" id="productPrice"></span></small></p>
-            </div>
-        <img class="card-img-bottom" id="productImg" src="" alt="alt text">
-        </div>
-        </a>`;
-
-    const createCard =
-    document.getElementById("productCards").innerHTML = productCard;
-}
-
-
- 
-
-
-
-
-
-window.onload =
+        const url = "http://localhost:3000/api/cameras"
     
-    createCards()
-    getCameras()
-    //displayData()
+            fetch(url)
+            .then(function(res) {
+                if (res.ok) {
+                    return res.json()
+                }
+            })
+            .then(function(data) {
+                
+                let produit = '<div class="row row-cols-1 row-cols-lg-2 g-4 text-center">';
+                for(let camera of data) {
+                    produit += // Création card HTML pour produits
+                    `
+                    
+                    <div class="card"> 
+                        <img src="${camera.imageUrl}" class="card-img-top" alt="teste alternatif"/>
+                        <div class="card-body">
+                            <h5 class="card-title">${camera.name}</h5>
+                            <p class="card-text">${camera.description}</p>
+                            <a href="#!" class="btn btn-warning">En savoir plus</a>
+                        </div>
+                    </div>`;
+                }
+                produit +='</div>';
+                document.querySelector("#produits").innerHTML = produit;
+
+    
+            })
+            
+
+    }  
+getCameras()
+
