265203eeda80997d7d547c385e0c36a3331586e4
Mivock <76434164+Mivock@users.noreply.github.com>
2021-05-27 11:56:56 +0200

    Création de la page index avec JS / premiers pas en javascript

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+/*
+
 
 // Récuperer les données de l'API
 
@@ -37,3 +39,75 @@
     }  
 getCameras()
 
+
+
+*/
+
+//définition url
+const url = "http://localhost:3000/api/cameras";
+
+// get
+async function getAPI() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      showData(data);
+      console.log(data[1]._id); //verif si c'est ok
+    });
+}
+
+//classe pour data de l'api
+/*
+class Data {
+  constructor(lenses, _id, name, price, description, imageUrl) {
+    this.lenses = lenses;
+    this.id = _id;
+    this.name = name;
+    this.price = price;
+    this.description = description;
+    this.image = imageUrl;
+  }
+}
+*/
+
+//conversion du prix en euros, fonction appelé dans showData() pour création de lu html
+function priceConversion(productPrice) {
+  let price = `${productPrice}`; //
+  price = Intl.NumberFormat("fr-FR", {
+    style: "currency",
+    currency: "EUR",
+    minimumFractionDigits: 2,
+  }).format(price / 100);
+  return price;
+}
+
+//creation affichage dans html
+function showData(data) {
+  let produit = '<div class="row row-cols-1 row-cols-lg-2 g-4 text-center">'; // mettre dans l'html
+  for (let camera of data) {
+    let price = priceConversion(camera.price);
+    console.log(priceConversion(camera.price));
+
+    produit +=
+      // Création card HTML pour produits
+      `
+                    
+                    <div class="card"> 
+                        <img src="${
+                          camera.imageUrl
+                        }" class="card-img-top" alt="teste alternatif"/>
+                        <div class="card-body">
+                            <h5 class="card-title">${camera.name}</h5>
+                            <p class="card-text">${priceConversion(
+                              //à rajouter pour activer la fonction
+                              camera.price
+                            )}</p> 
+                            <a href="#!" class="btn btn-warning">En savoir plus</a>
+                        </div>
+                    </div>`;
+  }
+  produit += "</div>";
+  document.querySelector("#produits").innerHTML = produit;
+}
+
+getAPI();
---
+++ js/item.js
@@ -0,0 +1,7 @@
+// partie produit avec URLSearchParams
+
+//recuperer les données de l'api encore une fois ? oui
+
+//changer l'url avec window.location (verifier)
+
+//refaire une fonction pour ajouter du html avec les valeurs de l'appareil photo séléctionné (chercher pour les lenses) -> voir select
