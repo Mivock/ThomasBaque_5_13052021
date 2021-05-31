027734dc32c4117a1c35b0f445f6ce0cc69541d4
Mivock <76434164+Mivock@users.noreply.github.com>
2021-05-31 12:36:36 +0200

    Rajout option lenses dans page produit

--- js/item.js
+++ js/item.js
@@ -1,13 +1,14 @@
 async function getAPI() {
   let cameraID = new URLSearchParams(location.search)
     .toString()
-    .substr(-25, 24);
-  //console.log(cameraID.substr(-25, 24)); //pour retirer le dernier caractère (=)
+    .substr(-25, 24); //pour retirer le dernier caractère (=)
+  //console.log(cameraID.substr(-25, 24));
   fetch(APIurl + `${cameraID /*.substr(-25, 24)*/}`)
     .then((response) => response.json())
     .then((data) => {
       createProduct(data);
-      console.log(APIurl + `${cameraID}`); //pour test;
+      //console.log(APIurl + `${cameraID}`); //pour test;
+      lenseOption(data);
     });
 }
 
@@ -19,22 +20,29 @@ function createProduct(data) {
     <h4 class="card-title">${data.name}</h4>
     <p class="card-text">${data.description}</p>
     <div class="options">
-      <label for="lense">Choisissez un objectif:</label>
-      <select name="lense" id="lense">
-        <option value="volvo">test</option>
-        <option value="saab">test</option>
-        <option value="saab">test</option>
-      </select>
+    <label for="lense">Choisissez un objectif:</label>
+    <select name="lense" id="lenses">
+    </select>
     </div>
     <div class="buy d-flex justify-content-between align-items-center">
       <div class="price text-success">
         <h5 class="mt-4">${priceConversion(data.price)}</h5>
       </div>
-      <a href="#" class="btn btn-danger mt-3">Ajouter au panier</a>
+      <a href="#" class="btn btn-danger mt-3" id="buy">Ajouter au panier</a>
     </div>
   </div>
 </div>`;
-  console.log(data.name); //pour test;
+  //console.log(data.name); //pour test;
+}
+
+function lenseOption(data) {
+  for (let lenses of data.lenses) {
+    document.querySelector("#lenses").innerHTML +=
+      //pas oublier le += pour rajouter tous les objectifs
+      ` 
+    <option value="${lenses}">${lenses}</option>
+  `; //html coupé de la fonction creatProduct, penser à mettre cette fontion en dernier sinon html pas encore crée et querySelector ne trouve pas #lenses
+  }
 }
 
 getAPI();
